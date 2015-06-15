438c79e0443f2d5322924a4687e40231a5d6a460
Robert Vrabel <rvrabel@gmail.com>
2015-06-15 10:27:29 -0400

    Fixing wrong package name from the previous repo. Updated the gulp watch to watch the correct file.

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('watch', ['styles'], function () {
     // Initialize livereload
     $.livereload = $.livereload();
 
-    gulp.watch('main.css', function (file) {
+    gulp.watch(targetAppDir + '403.css', function (file) {
         $.livereload.changed(file.path);
     });
 
